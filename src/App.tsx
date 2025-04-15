f75aa41da5c03a3787d042ba4b1d27c4dfa99eb0
RenataGaraeva <renatagaraeva12@gmail.com>
2025-04-16 02:14:10 +0300

    added filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import AddTaskForm from "./Components/AddTaskForm/AddTaskForm.tsx";
+import Filters from "./Components/Filters/Filters.tsx";
 
 function App() {
 
   return (
     <>
       <AddTaskForm />
+        <Filters />
     </>
   )
 }
---
+++ src/Components/Filters/Filters.tsx
@@ -0,0 +1,11 @@
+export default function Filters() {
+
+    return (
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+            <button>Clear completed</button>
+        </div>
+    );
+}
